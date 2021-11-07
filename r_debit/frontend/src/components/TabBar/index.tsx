113c185bfc50192e6aade2387c6ba51ca4eeb03f
AshishMadhu <ashishmadhu26@gmail.com>
2021-11-07 09:31:00 +0530

    updates

---
+++ r_debit/frontend/src/components/TabBar/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TabBar extends Component {
+    render() {
+        return (
+            <div>
+                <menu className="menu">
+                    
+                </menu>
+            </div>
+        );
+    }
+}
+
+export default TabBar;
